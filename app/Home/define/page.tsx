1473d82cdb8c5929112f463460aa015d9170ca5d
asmarsyed <asmarsyed@gmail.com>
2023-10-05 13:13:42 +0200

    define phase v2

--- app/Home/define/page.tsx
+++ app/Home/define/page.tsx
@@ -1,4 +1,8 @@
+"use client"
+
 import React from 'react'
+import Link from 'next/link'
+
 
 const Define = () => {
   return (
@@ -14,19 +18,132 @@ const Define = () => {
         1. Defining the problem : What challenge or opportunity do you seek to address? It's about pinpointing the core problem that needs solving or the process that requires optimization.
       </p>
 
-      <input className="w-full mt-4 border border-gray-400 rounded-xl p-2 shadow-md" type="text" placeholder="Define the problem"/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder="Define the problem"/>
 
       <button className="transition-all ease-in-out mt-4 border bg-blue-500 text-white rounded-xl p-2 hover:bg-blue-700">Next step</button>
 
       <p className="text-gray-500 mt-4">
-        2. Brainstorming how to achieve the project's goal or how to find a solution to the problem. This step involves defining the Xs (solutions) for the Y (the problem) in the equation Y = f(X).
+        2. Identifying what the customers want and serving priorities to their needs. That is what we call the Voice Of the Customer (VOC)
       </p>
 
-      <table>
-        
-      </table>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder="Voice Of the Customer (VOC)"/>
 
       <button className="transition-all ease-in-out mt-4 border bg-blue-500 text-white rounded-xl p-2 hover:bg-blue-700">Next step</button>
+
+      <p className="text-gray-500 mt-4">
+        3. Defining the project objective. 
+      </p>
+
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder="Project objective"/>
+
+      <button className="transition-all ease-in-out mt-4 border bg-blue-500 text-white rounded-xl p-2 hover:bg-blue-700">Next step</button>
+
+      <p className="text-gray-500 mt-4">
+        4. To define the project's scope, this step involves using the SIPOC tool, which stands for Supplier, Input, Process, Output, Customer.  SIPOC is a visual framework commonly used in process improvement, breaking down a process into key components for a high-level understanding.
+      </p>
+
+      <div className="grid grid-cols-5 gap-1">
+
+      <div className="w-full mt-4 border bg-gray-200 border-gray-400 p-2 shadow-md text-gray-600 font-bold text-center">Supplier</div>
+      <div className="w-full mt-4 border bg-gray-200 border-gray-400 p-2 shadow-md text-gray-600 font-bold text-center">Input</div>
+      <div className="w-full mt-4 border bg-gray-200 border-gray-400 p-2 shadow-md text-gray-600 font-bold text-center">Process</div>
+      <div className="w-full mt-4 border bg-gray-200 border-gray-400 p-2 shadow-md text-gray-600 font-bold text-center">Output</div>
+      <div className="w-full mt-4 border bg-gray-200 border-gray-400 p-2 shadow-md text-gray-600 font-bold text-center">Customer</div>
+
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder=""/>
+      
+
+      </div>
+
+      <button className="transition-all ease-in-out mt-4 border w-full bg-gray-300 text-gray-600 rounded-xl p-2 hover:bg-gray-400">+Add more</button>
+
+      <button className="transition-all ease-in-out mt-4 border bg-blue-500 text-white rounded-xl p-2 hover:bg-blue-700">Next step</button>
+
+      <p className="text-gray-500 mt-4">
+        Define now the scope of the project
+      </p>
+
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder="In Scope"/>
+
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder="Out Scope"/>
+
+      <button className="transition-all ease-in-out mt-4 border bg-blue-500 text-white rounded-xl p-2 hover:bg-blue-700">Next step</button>
+
+      <p className="text-gray-500 mt-4">
+        5. Defining the business case, which involves specifying the budget and calculating the potential cost savings achievable through this project. This financial aspect clarifies the project's economic impact and feasibility.
+      </p>
+
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder="Budget"/>
+
+      <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder="Savings"/>
+
+      <button className="transition-all ease-in-out mt-4 border w-full bg-gray-300 text-gray-600 rounded-xl p-2 hover:bg-gray-400">+Add more</button>
+
+      <button className="transition-all ease-in-out mt-4 border bg-blue-500 text-white rounded-xl p-2 hover:bg-blue-700">Next step</button>
+
+      <p className="text-gray-500 mt-4">
+        6. Defining the team for your DMAIC project :
+      </p>
+
+      <div className="flex w-full">
+        <div className="w-1/4 mt-4">
+          <div className="p-2 border border-gray-400 ">Team leader</div>
+          <div className="p-2 border border-gray-400 ">Team members</div>
+          <div className="p-2 border border-gray-400 ">Sponsor</div>
+          <div className="p-2 border border-gray-400 ">Stakeholders</div>
+        </div>
+
+        <div className="flex flex-col w-3/4 mt-4">
+        <input className="border border-gray-400 p-2" type="text" placeholder=""/>
+        <input className="border border-gray-400 p-2" type="text" placeholder=""/>
+        <input className="border border-gray-400 p-2" type="text" placeholder=""/>
+        <input className="border border-gray-400 p-2" type="text" placeholder=""/>
+        </div>
+      </div>
+      <button className="transition-all ease-in-out mt-4 border w-full bg-gray-300 text-gray-600 rounded-xl p-2 hover:bg-gray-400">+Add more</button>
+
+      <button className="transition-all ease-in-out mt-4 border bg-blue-500 text-white rounded-xl p-2 hover:bg-blue-700">Next step</button>
+
+      <p className="text-gray-500 mt-4">
+        The last step of the Define phase is to plan your project.
+      </p>
+
+      
+
+      <Link href="/Home/planning">
+        <button className="transition-all ease-in-out mt-4 border w-full bg-gray-300 text-gray-600 rounded-xl p-2 hover:bg-gray-400 mb-8">Plan the project</button>
+      </Link>
+
+
 
     </div>
   )
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React, { useState, useEffect, useCallback, useRef, createContext, useContext } from 'react';
-import Link from 'next/link'
+import Link from 'next/link'    
 
 const NavbarContext = createContext({})
 
@@ -10,8 +10,10 @@ export default function Navbar() {
 
 
 return (
-    <>
-    <aside className={`h-screen border-r border-gray-200 ${expanded ? "w-[222px] transition-all ease-in-out" : "w-[86px] transition-all ease-in-out"}`}>
+    <div className="flex">
+    <div className={`h-screen ${expanded ? "w-[222px] transition-all ease-in-out" : "w-[86px] transition-all ease-in-out"}`}></div>
+
+    <aside className={`fixed h-screen border-r border-gray-200 ${expanded ? "w-[222px] transition-all ease-in-out" : "w-[86px] transition-all ease-in-out"}`}>
         <nav>
             <div className="p-4 pb-2 flex justify-between items-center mb-2">
                 {expanded ?
@@ -34,7 +36,7 @@ return (
             </div>
             <ul>
                 <Link href="/Home/planning">
-                    <li className={`pl-2 w-[205px] rounded-md ml-2 mr-2 mb-2 flex ${expanded ? "hover:bg-gray-100 " : ""}`}>
+                    <li className={`pl-2 w-[205px] rounded-md ml-2 mr-2 mb-2 flex ${expanded ? "hover:bg-gray-100 " : "w-0"}`}>
                     <div className={`px-2 py-1 ${expanded ? "" : "transition-all ease-in-out hover:bg-gray-100 text-white rounded-md"}`}>
                     <svg className={`m-2 w-[20px] h-[24px] text-gray-500 ${expanded ? "" : ""}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1v3m5-3v3m5-3v3M1 7h7m1.506 3.429 2.065 2.065M19 7h-2M2 3h16a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1Zm6 13H6v-2l5.227-5.292a1.46 1.46 0 0 1 2.065 2.065L8 16Z"/>
@@ -47,7 +49,7 @@ return (
                     </li>
                 </Link>
                 <Link href="/Home/dashboard">
-                    <li className={`pl-2 w-[205px] rounded-md ml-2 mr-2 mb-2 flex ${expanded ? "hover:bg-gray-100 " : ""}`}>
+                    <li className={`pl-2 w-[205px] rounded-md ml-2 mr-2 mb-2 flex ${expanded ? "hover:bg-gray-100 " : "w-0"}`}>
                     <div className={`px-2 py-1 ${expanded ? "" : "hover:bg-gray-100 rounded-md"}`}>
                     <svg className={`m-2 w-[20px] h-[24px] text-gray-500 ${expanded ? "" : ""}`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1v14h16m0-9-3-2-3 5-3-2-3 4"/>
@@ -60,7 +62,7 @@ return (
                     </li>
                 </Link>
                 <Link href="/Home/define">
-                    <li className={`pl-2 w-[205px] rounded-md ml-2 mr-2 mb-2 flex ${expanded ? "hover:bg-gray-100 " : ""}`}>
+                    <li className={`pl-2 w-[205px] rounded-md ml-2 mr-2 mb-2 flex ${expanded ? "hover:bg-gray-100 " : "w-0"}`}>
                     <div className={`px-2 py-1 ${expanded ? "" : "hover:bg-gray-100 rounded-md"}`}>
                         <p className="flex w-5 h-6 m-2 font-bold text-gray-500 float-left text-center text-xl justify-center items-center">
                             D
@@ -74,7 +76,7 @@ return (
                     </li>
                 </Link>
                 <Link href="/Home/measure">
-                    <li className={`pl-2 w-[205px] rounded-md ml-2 mr-2 mb-2 flex ${expanded ? "hover:bg-gray-100 " : ""}`}>
+                    <li className={`pl-2 w-[205px] rounded-md ml-2 mr-2 mb-2 flex ${expanded ? "hover:bg-gray-100 " : "w-0"}`}>
                     <div className={`px-2 py-1 ${expanded ? "" : "hover:bg-gray-100 rounded-md"}`}>
                         <p className="flex w-5 h-6 m-2 font-bold text-gray-500 float-left text-center text-xl justify-center items-center">
                             M
@@ -88,7 +90,7 @@ return (
                     </li>
                 </Link>
                 <Link href="/Home/analyze">
-                    <li className={`pl-2 w-[205px] rounded-md ml-2 mr-2 mb-2 flex ${expanded ? "hover:bg-gray-100 " : ""}`}>
+                    <li className={`pl-2 w-[205px] rounded-md ml-2 mr-2 mb-2 flex ${expanded ? "hover:bg-gray-100 " : "w-0"}`}>
                     <div className={`px-2 py-1 ${expanded ? "" : "hover:bg-gray-100 rounded-md"}`}>
                         <p className="flex w-5 h-6 m-2 font-bold text-gray-500 float-left text-center text-xl justify-center items-center">
                             A
@@ -102,7 +104,7 @@ return (
                     </li>
                 </Link>
                 <Link href="/Home/improve">
-                    <li className={`pl-2 w-[205px] rounded-md ml-2 mr-2 mb-2 flex ${expanded ? "hover:bg-gray-100 " : ""}`}>
+                    <li className={`pl-2 w-[205px] rounded-md ml-2 mr-2 mb-2 flex ${expanded ? "hover:bg-gray-100 " : "w-0"}`}>
                     <div className={`px-2 py-1 ${expanded ? "" : "hover:bg-gray-100 rounded-md"}`}>
                         <p className="flex w-5 h-6 m-2 font-bold text-gray-500 float-left text-center text-xl justify-center items-center">
                             I
@@ -116,7 +118,7 @@ return (
                     </li>
                 </Link>
                 <Link href="/Home/control">
-                    <li className={`pl-2 w-[205px] rounded-md ml-2 mr-2 mb-2 flex ${expanded ? "hover:bg-gray-100 " : ""}`}>
+                    <li className={`pl-2 w-[205px] rounded-md ml-2 mr-2 mb-2 flex ${expanded ? "hover:bg-gray-100 " : "w-0"}`}>
                     <div className={`px-2 py-1 ${expanded ? "" : "hover:bg-gray-100 rounded-md"}`}>
                         <p className="flex w-5 h-6 m-2 font-bold text-gray-500 float-left text-center text-xl justify-center items-center">
                             C
@@ -132,6 +134,7 @@ return (
             </ul>
         </nav>
     </aside>
-    </>
+
+    </div>
 )
 }
