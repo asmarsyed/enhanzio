7e066906e7c53bf29ca8599cee4745a61fd09406
asmarsyed <asmarsyed@gmail.com>
2023-10-09 15:32:30 +0200

    error resolve 3

--- app/components/context/theme.ts
+++
@@ -1,9 +0,0 @@
-export const theme = {
-    expanded: {
-      marginLeft: '0px 0px 0px 222px',
-    },
-    notexpanded: {
-      marginLeft: '0px 0px 0px 86px',
-    },
-  };
-  
--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Link from 'next/link'
 import React, { useRef } from 'react'
+import Image from 'next/image'
 
 export default function Header() {
     return (
