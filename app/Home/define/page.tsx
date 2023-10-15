4c718c2a498bdd0f7aecaaf5a91d9c29f921673a
asmarsyed <asmarsyed@gmail.com>
2023-10-15 14:27:42 +0200

    process steps updated

--- app/Home/define/page.tsx
+++ app/Home/define/page.tsx
@@ -10,6 +10,24 @@ const Define = () => {
   const [expanded1, setExpanded1] = useState(1)
   const [submit1, setSubmit1] = useState(1)
 
+  const [expanded2, setExpanded2] = useState(1)
+  const [submit2, setSubmit2] = useState(1)
+
+  const [expanded3, setExpanded3] = useState(1)
+  const [submit3, setSubmit3] = useState(1)
+
+  const [expanded4, setExpanded4] = useState(1)
+  const [submit4, setSubmit4] = useState(1)
+
+  const [expanded5, setExpanded5] = useState(1)
+  const [submit5, setSubmit5] = useState(1)
+
+  const [expanded6, setExpanded6] = useState(1)
+  const [submit6, setSubmit6] = useState(1)
+
+  const [expanded7, setExpanded7] = useState(1)
+  const [submit7, setSubmit7] = useState(1)
+
   
 
   return (
@@ -36,7 +54,7 @@ const Define = () => {
         </button>
       </div>
 
-      <div id="ReactCollapse--collapse" className={`mt-2 w-full shadow-xl bg-white py-2 px-7 rounded-md  ${submit1 === 1 ? "transition ease-in-out delay-150" : "transition-all hidden"} ${expanded1 === 1 ? "hidden transition-opacity duration-500 ease-in-out opacity-0" : "transition-opacity duration-500 ease-in-out opacity-100"}`}>
+      <div className={`mt-2 w-full shadow-xl bg-white py-2 px-7 rounded-md  ${submit1 === 1 ? "transition ease-in-out delay-150" : "transition-all hidden"} ${expanded1 === 1 ? "hidden transition-opacity duration-500 ease-in-out opacity-0" : "transition-opacity duration-500 ease-in-out opacity-100"}`}>
 
 
       <p className="text-gray-500 mt-4">
@@ -48,17 +66,34 @@ const Define = () => {
       <button onClick={() => {
         setSubmit1(2);
         setExpanded1(3);
-      }} className="w-full text-center bg-indigo-900 hover:bg-indigo-700 p-2 rounded-md mb-2 mt-6 text-white">Submit</button>
+      }} className={`w-full text-center bg-indigo-900 hover:bg-indigo-700 p-2 rounded-md mb-2 mt-6 text-white`}>Submit</button>
 
 
       </div>
 
-      <div className="flex justify-between w-full shadow-xl bg-white p-2 rounded-md mt-4">
-        <p className="h-full flex justify-center items-center py-2 px-5">Voice Of the Customer</p>
-        <button className="bg-indigo-900 text-white py-2 px-10 rounded-md ">Start</button>
+      <div className={`flex justify-between w-full shadow-xl  p-2 rounded-md mt-4 ${ expanded1 === 3 ? "bg-white transition-colors" : "bg-gray-400" } `}>
+        <p className={`h-full flex justify-center items-center py-2 px-5 ${ expanded1 === 3 ? "" : "text-gray-500" } `}>Voice Of the Customer</p>
+
+        { expanded1 === 3 ?
+
+          <button onClick={() => { setSubmit2(1); setExpanded2(2) }} className={`text-white py-2 px-10 rounded-md ${expanded2 === 1 ? "bg-indigo-900 hover:bg-indigo-700 transition-colors" : expanded2 === 2 ? "bg-orange-800 transition-colors" : "bg-green-800 hover:bg-green-600 transition-colors"}}`}>
+          { expanded2 === 1 ?
+          <p>Start</p>
+          : expanded2 === 2 ?
+          <p>In progress</p>
+          :
+          <p>Done</p>
+          }
+          </button>
+
+        :
+        <p></p>
+
+        }     
+
       </div>
 
-      <div className="mt-2 w-full shadow-xl bg-white py-2 px-7 rounded-md hidden">
+      <div className={`mt-2 w-full shadow-xl bg-white py-2 px-7 rounded-md  ${submit2 === 1 ? "transition ease-in-out delay-150" : "transition-all hidden"} ${expanded2 === 1 ? "hidden transition-opacity duration-500 ease-in-out opacity-0" : "transition-opacity duration-500 ease-in-out opacity-100"}`}>
 
       <p className="text-gray-500 mt-4">
         2. Identifying what the customers want and serving priorities to their needs. That is what we call the Voice Of the Customer
@@ -66,35 +101,75 @@ const Define = () => {
       <input className="w-full mt-4 border border-gray-400 rounded-md p-2 shadow-md" type="text" placeholder="Voice Of the Customer (VOC)"/>
       </p>
 
-      <button className="w-full text-center bg-indigo-900 p-2 rounded-md mb-2 mt-6 text-white">Submit</button>
+      <button onClick={() => {
+        setSubmit2(2);
+        setExpanded2(3);
+      }} className={`w-full text-center bg-indigo-900 hover:bg-indigo-700 p-2 rounded-md mb-2 mt-6 text-white`}>Submit</button>
 
       </div>
 
 
-      <div className="flex justify-between w-full shadow-xl bg-white p-2 rounded-md mt-4">
-        <p className="h-full flex justify-center items-center py-2 px-5">Defining the project objective</p>
-        <button className="bg-indigo-900 text-white py-2 px-10 rounded-md ">Start</button>
+      <div className={`flex justify-between w-full shadow-xl  p-2 rounded-md mt-4 ${ expanded2 === 3 ? "bg-white transition-colors" : "bg-gray-400" } `}>
+        <p className={`h-full flex justify-center items-center py-2 px-5 ${ expanded2 === 3 ? "" : "text-gray-500" } `}>Defining the project objective</p>
+
+        { expanded2 === 3 ?
+
+          <button onClick={() => { setSubmit3(1); setExpanded3(2) }} className={`text-white py-2 px-10 rounded-md ${expanded3 === 1 ? "bg-indigo-900 hover:bg-indigo-700 transition-colors" : expanded3 === 2 ? "bg-orange-800 transition-colors" : "bg-green-800 hover:bg-green-600 transition-colors"}}`}>
+          { expanded3 === 1 ?
+          <p>Start</p>
+          : expanded3 === 2 ?
+          <p>In progress</p>
+          :
+          <p>Done</p>
+          }
+          </button>
+
+        :
+        <p></p>
+
+        }     
+
       </div>
 
-      <div className="mt-2 w-full shadow-xl bg-white py-2 px-7 rounded-md hidden">
+      <div className={`mt-2 w-full shadow-xl bg-white py-2 px-7 rounded-md  ${submit3 === 1 ? "transition ease-in-out delay-150" : "transition-all hidden"} ${expanded3 === 1 ? "hidden transition-opacity duration-500 ease-in-out opacity-0" : "transition-opacity duration-500 ease-in-out opacity-100"}`}>
 
       <p className="text-gray-500 mt-4">
-        3. Defining the project objective. 
+       3. Defining the project objective. 
 
       <input className="w-full mt-4 border border-gray-400 rounded-md p-2 shadow-md" type="text" placeholder="Project objective"/>
       </p>
 
-      <button className="w-full text-center bg-indigo-900 p-2 rounded-md mb-2 mt-6 text-white">Submit</button>
+      <button onClick={() => {
+        setSubmit3(2);
+        setExpanded3(3);
+      }} className={`w-full text-center bg-indigo-900 hover:bg-indigo-700 p-2 rounded-md mb-2 mt-6 text-white`}>Submit</button>
 
       </div>
 
 
-      <div className="flex justify-between w-full shadow-xl bg-white p-2 rounded-md mt-4">
-        <p className="h-full flex justify-center items-center py-2 px-5">Project scope</p>
-        <button className="bg-indigo-900 text-white py-2 px-10 rounded-md ">Start</button>
+      <div className={`flex justify-between w-full shadow-xl  p-2 rounded-md mt-4 ${ expanded3 === 3 ? "bg-white transition-colors" : "bg-gray-400" } `}>
+        <p className={`h-full flex justify-center items-center py-2 px-5 ${ expanded3 === 3 ? "" : "text-gray-500" } `}>Project scope</p>
+
+        { expanded3 === 3 ?
+
+          <button onClick={() => { setSubmit4(1); setExpanded4(2) }} className={`text-white py-2 px-10 rounded-md ${expanded4 === 1 ? "bg-indigo-900 hover:bg-indigo-700 transition-colors" : expanded4 === 2 ? "bg-orange-800 transition-colors" : "bg-green-800 hover:bg-green-600 transition-colors"}}`}>
+          { expanded4 === 1 ?
+          <p>Start</p>
+          : expanded4 === 2 ?
+          <p>In progress</p>
+          :
+          <p>Done</p>
+          }
+          </button>
+
+        :
+        <p></p>
+
+        }     
+
       </div>
 
-      <div className="mt-2 w-full shadow-xl bg-white py-2 px-7 rounded-md hidden">
+      <div className={`mt-2 w-full shadow-xl bg-white py-2 px-7 rounded-md  ${submit4 === 1 ? "transition ease-in-out delay-150" : "transition-all hidden"} ${expanded4 === 1 ? "hidden transition-opacity duration-500 ease-in-out opacity-0" : "transition-opacity duration-500 ease-in-out opacity-100"}`}>
 
       <p className="text-gray-500 mt-4">
         4. To define the project&apos;s scope, this step involves using the SIPOC tool, which stands for Supplier, Input, Process, Output, Customer.  SIPOC is a visual framework commonly used in process improvement, breaking down a process into key components for a high-level understanding.
@@ -152,43 +227,85 @@ const Define = () => {
 
       <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder="Out Scope"/>
 
-      <button className="w-full text-center bg-indigo-900 p-2 rounded-md mb-2 mt-6 text-white">Submit</button>
+      <button onClick={() => {
+        setSubmit4(2);
+        setExpanded4(3);
+      }} className={`w-full text-center bg-indigo-900 hover:bg-indigo-700 p-2 rounded-md mb-2 mt-6 text-white`}>Submit</button>
 
       </div>
 
-      
-      <div className="flex justify-between w-full shadow-xl bg-white p-2 rounded-md mt-4">
-        <p className="h-full flex justify-center items-center py-2 px-5">Business case</p>
-        <button className="bg-indigo-900 text-white py-2 px-10 rounded-md ">Start</button>
+
+      <div className={`flex justify-between w-full shadow-xl  p-2 rounded-md mt-4 ${ expanded4 === 3 ? "bg-white transition-colors" : "bg-gray-400" } `}>
+        <p className={`h-full flex justify-center items-center py-2 px-5 ${ expanded4 === 3 ? "" : "text-gray-500" } `}>Business case</p>
+
+        { expanded4 === 3 ?
+
+          <button onClick={() => { setSubmit5(1); setExpanded5(2) }} className={`text-white py-2 px-10 rounded-md ${expanded5 === 1 ? "bg-indigo-900 hover:bg-indigo-700 transition-colors" : expanded5 === 2 ? "bg-orange-800 transition-colors" : "bg-green-800 hover:bg-green-600 transition-colors"}}`}>
+          { expanded5 === 1 ?
+          <p>Start</p>
+          : expanded5 === 2 ?
+          <p>In progress</p>
+          :
+          <p>Done</p>
+          }
+          </button>
+
+        :
+        <p></p>
+
+        }     
+
       </div>
 
-      <div className="mt-2 w-full shadow-xl bg-white py-2 px-7 rounded-md hidden">
+      <div className={`mt-2 w-full shadow-xl bg-white py-2 px-7 rounded-md  ${submit5 === 1 ? "transition ease-in-out delay-150" : "transition-all hidden"} ${expanded5 === 1 ? "hidden transition-opacity duration-500 ease-in-out opacity-0" : "transition-opacity duration-500 ease-in-out opacity-100"}`}>
 
       <p className="text-gray-500 mt-4">
         5. Defining the business case, which involves specifying the budget and calculating the potential cost savings achievable through this project. This financial aspect clarifies the project&apos;s economic impact and feasibility.
       </p>
+
       <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder="Budget"/>
 
       <input className="w-full mt-4 border border-gray-400 rounded-sm p-2 shadow-md" type="text" placeholder="Savings"/>
 
       <button className="transition-all ease-in-out mt-4 border w-full bg-gray-300 text-gray-600 rounded-xl p-2 hover:bg-gray-400">+Add more</button>
 
-      <button className="w-full text-center bg-indigo-900 p-2 rounded-md mb-2 mt-6 text-white">Submit</button>
+      
+
+      <button onClick={() => {
+        setSubmit5(2);
+        setExpanded5(3);
+      }} className={`w-full text-center bg-indigo-900 hover:bg-indigo-700 p-2 rounded-md mb-2 mt-6 text-white`}>Submit</button>
 
       </div>
 
+      <div className={`flex justify-between w-full shadow-xl  p-2 rounded-md mt-4 ${ expanded5 === 3 ? "bg-white transition-colors" : "bg-gray-400" } `}>
+        <p className={`h-full flex justify-center items-center py-2 px-5 ${ expanded5 === 3 ? "" : "text-gray-500" } `}>Project team</p>
 
-      <div className="flex justify-between w-full shadow-xl bg-white p-2 rounded-md mt-4">
-        <p className="h-full flex justify-center items-center py-2 px-5">Project team</p>
-        <button className="bg-indigo-900 text-white py-2 px-10 rounded-md ">Start</button>
+        { expanded5 === 3 ?
+
+          <button onClick={() => { setSubmit6(1); setExpanded6(2) }} className={`text-white py-2 px-10 rounded-md ${expanded6 === 1 ? "bg-indigo-900 hover:bg-indigo-700 transition-colors" : expanded6 === 2 ? "bg-orange-800 transition-colors" : "bg-green-800 hover:bg-green-600 transition-colors"}}`}>
+          { expanded6 === 1 ?
+          <p>Start</p>
+          : expanded6 === 2 ?
+          <p>In progress</p>
+          :
+          <p>Done</p>
+          }
+          </button>
+
+        :
+        <p></p>
+
+        }     
+
       </div>
 
-      <div className="mt-2 w-full shadow-xl bg-white py-2 px-7 rounded-md hidden">
+      <div className={`mt-2 w-full shadow-xl bg-white py-2 px-7 rounded-md  ${submit6 === 1 ? "transition ease-in-out delay-150" : "transition-all hidden"} ${expanded6 === 1 ? "hidden transition-opacity duration-500 ease-in-out opacity-0" : "transition-opacity duration-500 ease-in-out opacity-100"}`}>
 
       <p className="text-gray-500 mt-4">
-        6. Defining the team for your DMAIC project :
+      6. Defining the team for your DMAIC project :
       </p>
-      
+
       <div className="flex w-full">
         <div className="w-1/4 mt-4">
           <div className="p-2 border border-gray-400 ">Team leader</div>
@@ -206,28 +323,47 @@ const Define = () => {
       </div>
       <button className="transition-all ease-in-out mt-4 border w-full bg-gray-300 text-gray-600 rounded-xl p-2 hover:bg-gray-400">+Add more</button>
 
-      <button className="w-full text-center bg-indigo-900 p-2 rounded-md mb-2 mt-6 text-white">Submit</button>
-
-      </div>
       
 
+      <button onClick={() => {
+        setSubmit6(2);
+        setExpanded6(3);
+      }} className={`w-full text-center bg-indigo-900 hover:bg-indigo-700 p-2 rounded-md mb-2 mt-6 text-white`}>Submit</button>
 
-      <div className="flex justify-between w-full shadow-xl bg-white p-2 rounded-md mt-4">
-        <p className="h-full flex justify-center items-center py-2 px-5">Plan</p>
-        <button className="bg-indigo-900 text-white py-2 px-10 rounded-md ">Start</button>
       </div>
 
-      <div className="mt-2 w-full shadow-xl bg-white py-2 px-7 rounded-md hidden">
+      
+      <div className={`flex justify-between w-full shadow-xl  p-2 rounded-md mt-4 ${ expanded6 === 3 ? "bg-white transition-colors" : "bg-gray-400" } `}>
+        <p className={`h-full flex justify-center items-center py-2 px-5 ${ expanded6 === 3 ? "" : "text-gray-500" } `}>Plan</p>
+
+        { expanded6 === 3 ?
+
+          <button onClick={() => { setSubmit7(1); setExpanded7(2) }} className={`text-white py-2 px-10 rounded-md ${expanded7 === 1 ? "bg-indigo-900 hover:bg-indigo-700 transition-colors" : expanded7 === 2 ? "bg-orange-800 transition-colors" : "bg-green-800 hover:bg-green-600 transition-colors"}}`}>
+          { expanded7 === 1 ?
+          <p>Start</p>
+          : expanded7 === 2 ?
+          <p>In progress</p>
+          :
+          <p>Done</p>
+          }
+          </button>
+
+        :
+        <p></p>
+
+        }     
+
+      </div>
+
+      <div className={`mt-2 w-full shadow-xl bg-white py-2 px-7 rounded-md  ${submit7 === 1 ? "transition ease-in-out delay-150" : "transition-all hidden"} ${expanded7 === 1 ? "hidden transition-opacity duration-500 ease-in-out opacity-0" : "transition-opacity duration-500 ease-in-out opacity-100"}`}>
 
       <p className="text-gray-500 mt-4">
       The last step of the Define phase is to plan your project.
       </p>
-      
+
       <Link href="/Home/planning">
         <button className="transition-all ease-in-out mt-4 border w-full bg-gray-300 text-gray-600 rounded-xl p-2 hover:bg-gray-400 mb-8">Plan the project</button>
       </Link>
-
-      <button className="w-full text-center bg-indigo-900 p-2 rounded-md mb-2 mt-6 text-white">Submit</button>
 
       </div>
 
