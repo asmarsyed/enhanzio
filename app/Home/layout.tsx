71133b1650095cdfda3cac13a462e82bb04e4084
asmarsyed <asmarsyed@gmail.com>
2023-10-18 10:33:36 +0200

    analytics

--- app/Home/layout.tsx
+++ app/Home/layout.tsx
@@ -1,25 +1,26 @@
-"use client"
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Home from './page'
-import Navbar from '../components/navbar'
-import Footer from '../components/footer'
+"use client";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Home from "./page";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <head>
-      </head>
+      <head></head>
       <body className="bg-white">
         <main className="flex">
           <Navbar />
           {children}
+          <Analytics />
         </main>
       </body>
     </html>
-  )
+  );
 }
