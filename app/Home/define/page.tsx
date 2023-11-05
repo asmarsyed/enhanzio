7ed164fcb1f1e63eed19851457d548467755c75e
asmarsyed <asmarsyed@gmail.com>
2023-11-05 21:45:41 +0100

    update 2

--- app/Home/define/page.tsx
+++ app/Home/define/page.tsx
@@ -14,8 +14,8 @@ import Activityupdate from "@/app/components/activity/activityupdate";
 const Define = () => {
   return (
     <div className="pt-[30px] mt-24 md:mx-10 mx-2 w-full flex transition-all border-xl">
-      <div className="w-full">
-        <div className="flex bg-white rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+      <div className="w-full rounded-xl shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px]">
+        <div className="flex bg-white rounded-xl">
           <Etapes
             stepnumber="1"
             stepname="Project Chart"
@@ -45,7 +45,7 @@ const Define = () => {
             stepactive={false}
           />
         </div>
-        <div>
+        <div className="">
           <Steps stepname="Defining the problem">
             <div className="flex w-full h-full">
               <div className="w-1/2 mr-[45px]">
@@ -55,49 +55,49 @@ const Define = () => {
                 <div className="flex justify-end items-center flex-auto mb-4">
                   <p className="mr-2 text-[18px]">What ?</p>
                   <input
-                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border"
+                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border border-[#CCCCCC]"
                     type="text"
                   />
                 </div>
                 <div className="flex justify-end items-center flex-auto mb-4">
                   <p className="mr-2 text-[18px]">Where ?</p>
                   <input
-                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border"
+                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border border-[#CCCCCC]"
                     type="text"
                   />
                 </div>
                 <div className="flex justify-end items-center flex-auto mb-4">
                   <p className="mr-2 text-[18px]">When ?</p>
                   <input
-                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border"
+                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border border-[#CCCCCC]"
                     type="text"
                   />
                 </div>
                 <div className="flex justify-end items-center flex-auto mb-4">
                   <p className="mr-2 text-[18px]">Who ?</p>
                   <input
-                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border"
+                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border border-[#CCCCCC]"
                     type="text"
                   />
                 </div>
                 <div className="flex justify-end items-center flex-auto mb-4">
                   <p className="mr-2 text-[18px]">Why ?</p>
                   <input
-                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border"
+                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border border-[#CCCCCC]"
                     type="text"
                   />
                 </div>
                 <div className="flex justify-end items-center flex-auto mb-4">
                   <p className="mr-2 text-[18px]">How ?</p>
                   <input
-                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border"
+                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border border-[#CCCCCC]"
                     type="text"
                   />
                 </div>
                 <div className="flex justify-end items-center flex-auto mb-4">
                   <p className="mr-2 text-[18px]">How much ?</p>
                   <input
-                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border"
+                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border border-[#CCCCCC]"
                     type="text"
                   />
                 </div>
@@ -107,7 +107,7 @@ const Define = () => {
                   Now define the problem briefly :
                 </p>
                 <div className="h-full">
-                  <textarea className="w-full h-[80%] p-2 rounded-md align-top border" />
+                  <textarea className="w-full h-[80%] p-2 rounded-md align-top border border-[#CCCCCC]" />
                 </div>
               </div>
             </div>
@@ -117,14 +117,14 @@ const Define = () => {
               <div className="w-full flex justify-end mb-4">
                 <p className="mr-4">In scope</p>
                 <input
-                  className="w-[90%] flex items-center p-2 rounded-md border h-[34px]"
+                  className="w-[90%] flex items-center p-2 rounded-md border border-[#CCCCCC] h-[34px]"
                   type="text"
                 />
               </div>
               <div className="w-full flex justify-end">
                 <p className="mr-4">Out scope</p>
                 <input
-                  className="w-[90%] flex items-center p-2 rounded-md border h-[34px]"
+                  className="w-[90%] flex items-center p-2 rounded-md border border-[#CCCCCC] h-[34px]"
                   type="text"
                 />
               </div>
@@ -139,35 +139,35 @@ const Define = () => {
                 <div className="flex justify-end items-center flex-auto mb-4">
                   <p className="mr-2 text-[18px]">Specific</p>
                   <input
-                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border"
+                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border border-[#CCCCCC]"
                     type="text"
                   />
                 </div>
                 <div className="flex justify-end items-center flex-auto mb-4">
                   <p className="mr-2 text-[18px]">Measurable</p>
                   <input
-                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border"
+                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border border-[#CCCCCC]"
                     type="text"
                   />
                 </div>
                 <div className="flex justify-end items-center flex-auto mb-4">
                   <p className="mr-2 text-[18px]">Achievable</p>
                   <input
-                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border"
+                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border border-[#CCCCCC]"
                     type="text"
                   />
                 </div>
                 <div className="flex justify-end items-center flex-auto mb-4">
                   <p className="mr-2 text-[18px]">Relevant</p>
                   <input
-                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border"
+                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border border-[#CCCCCC]"
                     type="text"
                   />
                 </div>
                 <div className="flex justify-end items-center flex-auto mb-4">
                   <p className="mr-2 text-[18px]">Time-based</p>
                   <input
-                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border"
+                    className="h-[34px] w-[75%] flex items-center p-2 rounded-md border border-[#CCCCCC]"
                     type="text"
                   />
                 </div>
@@ -177,55 +177,55 @@ const Define = () => {
                   Now define the problem briefly :
                 </p>
                 <div className="h-full">
-                  <textarea className="w-full h-[75%] p-2 rounded-md align-top border" />
+                  <textarea className="w-full h-[75%] p-2 rounded-md align-top border border-[#CCCCCC]" />
                 </div>
               </div>
             </div>
           </Steps>
           <Steps stepname="Business case">
-            <div className="flex h-full w-full">
-              <div className="w-1/3">
-                <p className="h-1/3 flex  items-center border-b border-slate-300 mb-2">
-                  Gain
-                </p>
-                <p className="h-1/3 flex items-center mb-2">Budget</p>
-                <p className="h-1/3 flex items-center ">Savings</p>
+            <div className="w-full mt-2 text-[18px]">
+              <div className="flex w-full border-b border-[#CCCCCC] mb-2">
+                <p className="w-1/3">Gain</p>
+                <p className="w-1/3">Current value</p>
+                <p className="w-1/3">Target value</p>
               </div>
-              <div className="w-1/3">
-                <p className="h-1/3 flex items-center mb-2 border-b border-slate-300">
-                  Current value
-                </p>
-                <div className="h-1/3 flex mr-2 items-center mb-2">
+              <div className="flex w-full mb-2">
+                <p className="w-1/3">Budget</p>
+                <div className="w-1/3 mr-2">
                   <input
-                    className="h-[34px] w-full flex items-center p-2 rounded-md border"
+                    className="w-full border border-[#CCCCCC] rounded-md h-[34px] flex items-center p-2"
                     type="text"
                   />
                 </div>
-                <div className="h-1/3 flex mr-2 items-center mb-2">
+                <div className="w-1/3">
                   <input
-                    className="h-[34px] w-full flex items-center p-2 rounded-md border"
+                    className="w-full border border-[#CCCCCC] rounded-md h-[34px] flex items-center p-2"
                     type="text"
                   />
                 </div>
               </div>
-              <div className="w-1/3">
-                <p className="h-1/3 flex items-center mb-2 border-b border-slate-300">
-                  Target value
-                </p>
-                <div className="h-1/3 flex items-center mb-2">
+              <div className="flex w-full">
+                <p className="w-1/3">Savings</p>
+                <div className="w-1/3 mr-2">
                   <input
-                    className="h-[34px] w-full flex items-center p-2 rounded-md border"
+                    className="w-full border border-[#CCCCCC] rounded-md h-[34px] flex items-center p-2"
                     type="text"
                   />
                 </div>
-                <div className="h-1/3 flex items-center mb-2">
+                <div className="w-1/3">
                   <input
-                    className="h-[34px] w-full flex items-center p-2 rounded-md border"
+                    className="w-full border border-[#CCCCCC] rounded-md h-[34px] flex items-center p-2"
                     type="text"
                   />
                 </div>
               </div>
             </div>
+            <button className="mt-4 text-[18px] bg-[#001F3E] rounded-xl w-full text-white h-10">
+              Add more+
+            </button>
+          </Steps>
+          <Steps stepname="Project team">
+            
           </Steps>
         </div>
       </div>
--- app/components/activity/activityheader.tsx
+++ app/components/activity/activityheader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const history = () => {
   return (
-    <div className="flex flex-auto items-center px-8 py-3 m-0 h-[68px] rounded-xl bg-[#F3F7FC]">
+    <div className="flex flex-auto items-center px-8 py-3 m-0 h-[68px] rounded-t-xl bg-[#F3F7FC] border-b">
       <p className="font-bold text-[18px] text-[#001F3E]">Activity</p>
     </div>
   );
