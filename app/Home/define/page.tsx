f015d37a2994918797822f9a47bf3535a41b7841
asmarsyed <asmarsyed@gmail.com>
2023-11-06 22:51:13 +0100

    solving

--- app/Home/define/page.tsx
+++ app/Home/define/page.tsx
@@ -595,7 +595,7 @@ const Define = () => {
               </div>
               <div>
                 <p>Start date</p>
-                
+
               </div>
             </div>
           </Steps>
